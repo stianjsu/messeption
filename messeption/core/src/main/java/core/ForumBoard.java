162e3f8a6eefbb0b96d724edd988afb0b63af2ae
trygvaan <trygvaan@stud.ntnu.no>
2021-09-21 14:18:54 +0200

    Added basic functionality to ForumBoard core class

--- messeption/core/src/main/java/core/ForumBoard.java
+++ messeption/core/src/main/java/core/ForumBoard.java
@@ -1,5 +1,15 @@
 package core;
 
-public class ForumBoard {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ForumBoard {
+    private List<ForumPost> posts = new ArrayList<>();
+
+    public ForumBoard(){}
+
+    public List<ForumPost> getPosts() {return new ArrayList<>(posts);}
+    public ForumPost getPost(int i) {return posts.get(i);}
+
+    
 }
