08b2ad5e28ad7e4cf7dfc9aae7bd917c7881d6d2
trygvaan <trygvaan@stud.ntnu.no>
2021-09-21 14:21:26 +0200

    added newPost and deletePost method

--- messeption/core/src/main/java/core/ForumBoard.java
+++ messeption/core/src/main/java/core/ForumBoard.java
@@ -11,5 +11,12 @@ public class ForumBoard {
     public List<ForumPost> getPosts() {return new ArrayList<>(posts);}
     public ForumPost getPost(int i) {return posts.get(i);}
 
-    
+    public void newPost(String title, String text){
+        posts.add(new ForumPost(title, text));
+    }    
+
+    public void deletePost(ForumPost post){
+        posts.remove(post);
+    }
+
 }
