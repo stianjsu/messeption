d0b86ffa1a9b46c16cdc6dec1983ae23c468a19d
Jonah Niklas Bjørgsvik Wiecek <jnwiecek@stud.ntnu.no>
2021-09-22 08:47:54 +0200

    small changes to make merging easier

--- messeption/ui/src/main/java/ui/MesseptionApp.java
+++ messeption/ui/src/main/java/ui/MesseptionApp.java
@@ -1,5 +1,10 @@
 package ui;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
 public class MesseptionApp {
-    static int hei = 1;
+
 }
