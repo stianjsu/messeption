843503be385df0f0adf517098690df7e1c85e857
Mattias Lund Dahlin <mattiald@stud.ntnu.no>
2021-09-28 16:32:06 +0200

    Changed front page to support new post creation

--- messeption/ui/src/main/java/messeption/ui/FrontPageController.java
+++ messeption/ui/src/main/java/messeption/ui/FrontPageController.java
@@ -1,64 +1,90 @@
 package messeption.ui;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+
+
+
 import messeption.core.ForumBoard;
 import messeption.core.ForumPost;
 import messeption.core.JSONReadWrite;
 
 public class FrontPageController {
 
+    private static final int OFFSET = 230;
+    private final int POSITION = 15;
+
+    
     @FXML
-    Accordion postsContainer;
+    AnchorPane postsContainer;
     @FXML
     Button createPostButton;
-
+    @FXML
+    Pane postPane;
+    
     private ForumBoard forumBoard;
 
-    public void initialize() throws IOException {
-        forumBoard = JSONReadWrite.fileRead();
+    public void initialize(){
 
         drawPosts();
     }
 
+    public ForumBoard getBoard(){
+        return forumBoard;
+    }
+
     public void drawPosts() {
-        postsContainer.getPanes().clear();
+        try{
+            forumBoard = JSONReadWrite.fileRead();
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        
         List<ForumPost> posts = forumBoard.getPosts();
 
-        for (ForumPost post : posts) {
+        int i = 0;
+        for(ForumPost post : posts){
+            String title = post.getTitle();
+            String text = post.getText();
+            
+            Pane pane = generatePostPane(title, text);
+            pane.setLayoutY(POSITION+i*OFFSET);
+            pane.setLayoutX(POSITION);
 
-            TitledPane postContainer = new TitledPane();
-            postContainer.setAnimated(false);
-            postContainer.setText(post.getTitle());
-            postContainer.setFont(new Font(16));
-
-            AnchorPane postTextContainer = new AnchorPane();
-            postTextContainer.setMinHeight(0.0);
-            postTextContainer.setMinWidth(0.0);
-            postTextContainer.setPrefHeight(180.0);
-            postTextContainer.setPrefWidth(200.0);
-
-            TextArea postText = new TextArea();
-            postText.setPrefHeight(200.0);
-            postText.setPrefWidth(598.0);
-            postText.setText(post.getText());
-            postText.setDisable(true);
-            postText.setStyle("-fx-opacity: 1");
-
-            postTextContainer.getChildren().add(postText);
-            postContainer.setContent(postTextContainer);
-            postsContainer.getPanes().add(postContainer);
+            postsContainer.getChildren().add(pane);
+            i++;
         }
+        postsContainer.setPrefHeight((2 * POSITION + OFFSET) * i);
+    }
+
+    private Pane generatePostPane(String title, String text){
+        Pane pane = new Pane(postPane);
+    
+        pane.getChildren().forEach( p -> {
+            System.out.println(p.getClass());
+            if(p.getId().equals("titleLable") && p instanceof Label){
+                Label titleLabel = (Label) p;
+                titleLabel.setText(title);
+            }
+            else if(p.getId().equals("postTextArea") && p instanceof TextArea){
+                TextArea postTextArea = (TextArea) p;
+                postTextArea.setText(text);
+            }
+        });   
+        
+
+        return pane;
     }
 
 }
