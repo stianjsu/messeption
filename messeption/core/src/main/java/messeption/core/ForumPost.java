a227266288cc182a2c89de4b05a6bac68acd659f
Jonah Niklas Bjørgsvik Wiecek <jnwiecek@stud.ntnu.no>
2021-10-05 12:15:45 +0200

    added likes/dislikes with getters and increment
    Co-authored-by: Stian Sulebak <stianjsu@users.noreply.github.com>

--- messeption/core/src/main/java/messeption/core/ForumPost.java
+++ messeption/core/src/main/java/messeption/core/ForumPost.java
@@ -6,27 +6,47 @@ import java.time.format.DateTimeFormatter;
 public class ForumPost {
     private String title;
     private String text;
+    private int likes;
+    private int dislikes;
     private String timeStamp;
 
     public ForumPost(String title, String text) {
         this.title = title;
         this.text = text;
+        this.likes = 0;
+        this.dislikes = 0;
         this.timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy"));
     }
 
     public String getTitle() {
-        return title;
+        return this.title;
     }
 
     public String getText() {
-        return text;
+        return this.text;
+    }
+
+    public int getLikes(){
+        return this.likes;
+    }
+
+    public int getDislikes(){
+        return this.dislikes;
+    }
+
+    public void incrementLikes(){
+        this.likes++;
+    }
+
+    public void incrementDislikes(){
+        this.dislikes++;
     }
 
     public String getTimeStamp() {
-        return timeStamp;
+        return this.timeStamp;
     }
 
     public String toString() {
-        return "Title: " + title + "\tText: " + text + "\n TimeStamp: " + timeStamp;
+        return "Title: " + this.title + "\tText: " + this.text + "\n TimeStamp: " + this.timeStamp;
     }
 }
