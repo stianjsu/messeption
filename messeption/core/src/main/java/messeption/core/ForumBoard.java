ed0bdda9775930b0ed2f6f5b02c5f97aab480d70
Jonah Niklas Bjørgsvik Wiecek <jnwiecek@stud.ntnu.no>
2021-10-12 15:45:29 +0200

    Fixed checkstylewarning in board and post. Javadoc

--- messeption/core/src/main/java/messeption/core/ForumBoard.java
+++ messeption/core/src/main/java/messeption/core/ForumBoard.java
@@ -6,44 +6,114 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * ForumBoard has a collection of posts.
+ * 
+ */
 public class ForumBoard {
+  
   private List<ForumPost> posts = new ArrayList<>();
+  
+  /** 
+   * Getter for posts list.
 
-  public ForumBoard() {
-  }
-
+   * @return List of ForumPost
+   */
   public List<ForumPost> getPosts() {
     return new ArrayList<>(posts);
   }
 
+  
+  /** 
+   * Getter for specified post at index i.
+
+   * @param i index
+   * @return ForumPost at index i
+   */
   public ForumPost getPost(int i) {
     return posts.get(i);
   }
 
+  
+  /** 
+   * Creates a new post and adds it to the list posts.
+
+   * @param title title of post
+   * @param text text in post
+   */
   public void newPost(String title, String text) {
     posts.add(new ForumPost(title, text));
   }
 
+  
+  /** 
+   * Saves all the information in ForumBoard on a specified file at a specified path.
+   * Forumboard has a collection with the ForumPosts, hence the posts are saved.
+
+   * @param path filepath
+   * @param fileName filename
+   * @throws IOException fileError
+   */
   public void savePosts(String path, String fileName) throws IOException {
     JSONReadWrite.fileWrite(path, fileName, this);
   }
 
+  
+  /** 
+   * Saves all the information in ForumBoard on a default location.
+   * Forumboard has a collection with the ForumPosts, hence the posts are saved.
+
+   * @throws IOException fileError
+   */
   public void savePosts() throws IOException {
     JSONReadWrite.fileWrite(this);
   }
 
-  public void loadPosts(String path, String fileName) throws JsonSyntaxException, JsonIOException, IOException {
+  
+  /** 
+   * Loads the post from a file into the posts list from specified location.
+
+   * @param path filePath
+   * @param fileName fileName
+   * @throws JsonSyntaxException fileError
+   * @throws JsonIOException fileError
+   * @throws IOException fileError
+   */
+  public void loadPosts(String path, String fileName)
+        throws JsonSyntaxException, JsonIOException, IOException {
     posts = JSONReadWrite.fileRead(path, fileName).getPosts();
   }
 
+  
+  /** 
+   * Loads the post from a file into the posts list from default location.
+
+   * @throws JsonSyntaxException fileError
+   * @throws JsonIOException fileError
+   * @throws IOException fileError
+   */
   public void loadPosts() throws JsonSyntaxException, JsonIOException, IOException {
     posts = JSONReadWrite.fileRead().getPosts();
   }
 
+  
+  /** 
+   * Removed the given post from posts list.
+
+   * @param post ForumPost
+   * 
+   */
   public void deletePost(ForumPost post) {
     posts.remove(post);
   }
 
+  
+  /** 
+   * Uses ForumPosts toString method to create a string that consists of all
+   * the posts in posts-list.
+
+   * @return String
+   */
   public String toString() {
     StringBuilder s = new StringBuilder();
     for (ForumPost post : posts) {
@@ -51,5 +121,4 @@ public class ForumBoard {
     }
     return s.toString();
   }
-
 }
--- messeption/core/src/main/java/messeption/core/ForumPost.java
+++ messeption/core/src/main/java/messeption/core/ForumPost.java
@@ -3,27 +3,56 @@ package messeption.core;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Forum Post is a single post that has text and can have children, in form of comments.
+ */
 public class ForumPost extends UserTextSubmission {
   private String title;
   private List<PostComment> comments;
 
+  /**
+   * Constructur.
+   * Uses UserTextSubmissions constructur for text, likes/dislikes and time.
+   * Sets the title and initializes empty list of comments.
+
+   * @param title title of post
+   * @param text text in post
+   */
   public ForumPost(String title, String text) {
     super(text);
     this.title = title;
     this.comments = new ArrayList<>();
   }
 
+  
+  /** 
+   * Getter for title.
+
+   * @return Title of post
+   */
   public String getTitle() {
     return this.title;
   }
 
+  
+  /** 
+   * Getter for comments.
+
+   * @return List of comments
+   */
   public List<PostComment> getComments() {
     return new ArrayList<>(comments);
   }
 
+  
+  /** 
+   * Returns a string with information about all the fields in post.
+
+   * @return String
+   */
   @Override
   public String toString() {
-    return "Title: " + this.title + "\tText: " + this.text + "\nLikes: " + this.likes + "\t Dislikes: " + this.dislikes
-        + "\n TimeStamp: " + this.timeStamp;
+    return "Title: " + this.title + "\tText: " + this.text + "\nLikes: "
+          + this.likes + "\t Dislikes: " + this.dislikes + "\n TimeStamp: " + this.timeStamp;
   }
 }
