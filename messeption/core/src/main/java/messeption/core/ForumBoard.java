7c29ce3ed390cb7e779f390a765278a0cc52c6a7
stianjsu <stianjsu@gmail.com>
2021-10-13 15:33:39 +0200

    Added functions to better accomadate comments in core

--- messeption/core/src/main/java/messeption/core/ForumBoard.java
+++ messeption/core/src/main/java/messeption/core/ForumBoard.java
@@ -11,22 +11,24 @@ import java.util.List;
  * 
  */
 public class ForumBoard {
-  
-  private List<ForumPost> posts = new ArrayList<>();
-  
-  /** 
-   * Getter for posts list.
 
+  private List<ForumPost> posts = new ArrayList<>();
+
+  /**
+   * Getter for posts list.
+   * 
    * @return List of ForumPost
    */
   public List<ForumPost> getPosts() {
+    if (posts == null || this.posts.size() < 1) {
+      return new ArrayList<>();
+    }
     return new ArrayList<>(posts);
   }
 
-  
-  /** 
+  /**
    * Getter for specified post at index i.
-
+   * 
    * @param i index
    * @return ForumPost at index i
    */
@@ -34,23 +36,22 @@ public class ForumBoard {
     return posts.get(i);
   }
 
-  
-  /** 
+  /**
    * Creates a new post and adds it to the list posts.
-
+   * 
    * @param title title of post
-   * @param text text in post
+   * @param text  text in post
    */
   public void newPost(String title, String text) {
     posts.add(new ForumPost(title, text));
   }
 
-  
-  /** 
-   * Saves all the information in ForumBoard on a specified file at a specified path.
-   * Forumboard has a collection with the ForumPosts, hence the posts are saved.
-
-   * @param path filepath
+  /**
+   * Saves all the information in ForumBoard on a specified file at a specified
+   * path. Forumboard has a collection with the ForumPosts, hence the posts are
+   * saved.
+   * 
+   * @param path     filepath
    * @param fileName filename
    * @throws IOException fileError
    */
@@ -58,48 +59,43 @@ public class ForumBoard {
     JsonReadWrite.fileWrite(path, fileName, this);
   }
 
-  
-  /** 
-   * Saves all the information in ForumBoard on a default location.
-   * Forumboard has a collection with the ForumPosts, hence the posts are saved.
-
+  /**
+   * Saves all the information in ForumBoard on a default location. Forumboard has
+   * a collection with the ForumPosts, hence the posts are saved.
+   * 
    * @throws IOException fileError
    */
   public void savePosts() throws IOException {
     JsonReadWrite.fileWrite(this);
   }
 
-  
-  /** 
+  /**
    * Loads the post from a file into the posts list from specified location.
-
-   * @param path filePath
+   * 
+   * @param path     filePath
    * @param fileName fileName
    * @throws JsonSyntaxException fileError
-   * @throws JsonIOException fileError
-   * @throws IOException fileError
+   * @throws JsonIOException     fileError
+   * @throws IOException         fileError
    */
-  public void loadPosts(String path, String fileName)
-        throws JsonSyntaxException, JsonIOException, IOException {
+  public void loadPosts(String path, String fileName) throws JsonSyntaxException, JsonIOException, IOException {
     posts = JsonReadWrite.fileRead(path, fileName).getPosts();
   }
 
-  
-  /** 
+  /**
    * Loads the post from a file into the posts list from default location.
-
+   * 
    * @throws JsonSyntaxException fileError
-   * @throws JsonIOException fileError
-   * @throws IOException fileError
+   * @throws JsonIOException     fileError
+   * @throws IOException         fileError
    */
   public void loadPosts() throws JsonSyntaxException, JsonIOException, IOException {
     posts = JsonReadWrite.fileRead().getPosts();
   }
 
-  
-  /** 
+  /**
    * Removed the given post from posts list.
-
+   * 
    * @param post ForumPost
    * 
    */
@@ -107,11 +103,10 @@ public class ForumBoard {
     posts.remove(post);
   }
 
-  
-  /** 
-   * Uses ForumPosts toString method to create a string that consists of all
-   * the posts in posts-list.
-
+  /**
+   * Uses ForumPosts toString method to create a string that consists of all the
+   * posts in posts-list.
+   * 
    * @return String
    */
   public String toString() {
--- messeption/core/src/main/java/messeption/core/JSONReadWrite.java
+++ messeption/core/src/main/java/messeption/core/JSONReadWrite.java
@@ -16,27 +16,26 @@ import java.nio.file.Paths;
  * A class with static methods for writing ForumBoard objects to Json file.
  */
 public class JsonReadWrite {
-  public static final String ROOT_PATH = Paths.get("").toAbsolutePath().toString() 
-      + "/src/main/resources/messeption/";
+  public static final String ROOT_PATH = Paths.get("").toAbsolutePath().toString() + "/src/main/resources/messeption/";
   // ender med messeption/core/ eller messeption/ui/
   public static final String UI_PATH = "ui/";
   public static final String DEFAULT_BOARD_FILE = "Board.JSON";
 
   /**
    * Reads from a specific file and returns a ForumBoard object.
-
+   * 
    * @param filePath the path of the file to read from
    * @param fileName the name of the file to read from
    * @return the Board object from the read file
    * @throws JsonSyntaxException throws if the file does nnot have propoer syntax
-   * @throws JsonIOException throws if the Json operation has an IO exception
-   * @throws IOException throws in a regualr IO expetion is thrown
+   * @throws JsonIOException     throws if the Json operation has an IO exception
+   * @throws IOException         throws in a regualr IO expetion is thrown
    */
   public static ForumBoard fileRead(String filePath, String fileName)
       throws JsonSyntaxException, JsonIOException, IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    InputStreamReader reader = new InputStreamReader(
-        new FileInputStream(ROOT_PATH + filePath + fileName), StandardCharsets.UTF_8);
+    InputStreamReader reader = new InputStreamReader(new FileInputStream(ROOT_PATH + filePath + fileName),
+        StandardCharsets.UTF_8);
     ForumBoard toReturn = gson.fromJson(reader, ForumBoard.class);
     reader.close();
     return toReturn;
@@ -48,18 +47,17 @@ public class JsonReadWrite {
 
   /**
    * Writes a ForumBoard object to a Json file and a specific location.
-
+   * 
    * @param filePath the path of the file to read from
    * @param fileName the name of the file to read from
-   * @param board the object to be written
+   * @param board    the object to be written
    * @throws JsonIOException throws if the Json operation has an IO exception
-   * @throws IOException throws in a regualr IO expetion is thrown
+   * @throws IOException     throws in a regualr IO expetion is thrown
    */
-  public static void fileWrite(String filePath, String fileName, ForumBoard board) 
-      throws JsonIOException, IOException {
+  public static void fileWrite(String filePath, String fileName, ForumBoard board) throws JsonIOException, IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    OutputStreamWriter writer = new OutputStreamWriter(
-        new FileOutputStream(ROOT_PATH + filePath + fileName), StandardCharsets.UTF_8);
+    OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(ROOT_PATH + filePath + fileName),
+        StandardCharsets.UTF_8);
     gson.toJson(board, writer);
     writer.close();
   }
