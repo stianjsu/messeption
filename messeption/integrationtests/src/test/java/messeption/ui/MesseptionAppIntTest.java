22866ad06051a78711b8180ad63f4ded1678dd03
trygvaan <trygvaan@stud.ntnu.no>
2021-11-09 14:16:01 +0100

    feat(integration): added integrationtests module and configured server to run in terminal
    The integrationtests module has been configured with a pom and added to the root directory pom. Added basic web.xml and server logger. The server now runs int the terminal with localhost in browser.
    
    Closes #76
    Related to #74
    Development-method: individual programming

---
+++ messeption/integrationtests/src/test/java/messeption/ui/MesseptionAppIntTest.java
@@ -0,0 +1,10 @@
+package messeption.ui;
+
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+
+public class MesseptionAppIntTest extends ApplicationTest{
+  //TODO add tests
+}
+
