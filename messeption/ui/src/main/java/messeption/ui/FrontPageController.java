d36a2d456fff1b1529ac52276463ae95740a7f2a
Stian Jørstad Sulebak <stianjsu@stud.ntnu.no>
2021-11-18 20:56:51 +0100

    refactor(ui): frontPageController now uses PostPaneTemplateController for post generation
    Created PostPaneTemplateController in order to better generate posts on frontpage of app.
    FrontPageController now delegates the generation of posts to PostPaneTemplateController.
    
    Closes #96
    Development-method:Pair programming
    Co-authored-by: Jonah Niklas Bjørgsvik Wiecek
    <jnwiecek@stud.ntnu.no>

--- messeption/ui/src/main/java/messeption/ui/FrontPageController.java
+++ messeption/ui/src/main/java/messeption/ui/FrontPageController.java
@@ -23,7 +23,7 @@ import messeption.core.ForumPost;
 /**
  * Controller for the front page or main menu of the app.
  */
-public class FrontPageController {
+public class FrontPageController extends SceneController {
 
   private static final int SIZE_POSTS = 220;
   private static final int MARIGIN_TOP = 15;
@@ -56,15 +56,10 @@ public class FrontPageController {
   @FXML
   Button createPostButton;
 
-  private Stage primaryStage;
-  private BoardAccessInterface boardAccess;
-
   private PostPageController postPageController;
-  private Scene postPageScene;
 
   /**
    * Sets the onAction events of sortingButtons.
-   * 
 
    * @throws Exception If board cannot read form file
    */
@@ -84,8 +79,13 @@ public class FrontPageController {
     sortCommentCount.setOnAction((e) -> {
       sortPosts(sortCommentCount.getText());
     });
+    
+    this.createPostButton.setOnAction(event -> {
+      primaryStage.setScene(createPostPageScene);
+    });
   }
 
+  @Override
   public void setBoardAccess(BoardAccessInterface boardAccess) throws Exception {
     this.boardAccess = boardAccess;
     drawPosts();
@@ -94,12 +94,8 @@ public class FrontPageController {
   public void setPostPageController(PostPageController controller) {
     this.postPageController = controller;
   }
-  
-  public void setPostCommentsScene(Scene scene) {
-    postPageScene = scene;
-  }
 
-  private void sortPosts(String sortBy) {
+  void sortPosts(String sortBy) {
     
     try {
       List<ForumPost> posts = boardAccess.getPosts();
@@ -119,15 +115,17 @@ public class FrontPageController {
    */
   public void drawPosts() throws Exception {
     sortPosts("Time");
+    sortMenuButton.setText("Time");
   }
+
   /**
    * Draws the posts in the UI and makes them visible.
 
    * @throws Exception If board cannot read form file
    */
   public void drawPosts(List<ForumPost> posts) throws Exception {
-    scrollPane.setVvalue(0);
 
+    scrollPane.setVvalue(0);
     postsContainer.getChildren().clear();
 
     int indexId = 0;
@@ -135,134 +133,25 @@ public class FrontPageController {
 
       Pane pane = generatePostPane(post);
       pane.setLayoutY(MARIGIN_TOP + indexId * (SIZE_POSTS + MARIGIN_POSTS));
-      postsContainer.getChildren().add(pane);
+      postsContainer.getChildren().add(pane); 
 
       indexId++;
     }
     postsContainer.setPrefHeight(indexId * (SIZE_POSTS + MARIGIN_POSTS));
   }
 
+
   private Pane generatePostPane(ForumPost post) throws IOException {
 
-    Pane toReturn = FXMLLoader.load(getClass().getResource("PostPaneTemplate.fxml"));
-    List<Node> tempChildren = new ArrayList<>(toReturn.getChildren());
-    toReturn.getChildren().clear();
+    FXMLLoader postPaneTemplateLoader = new FXMLLoader(getClass().getResource(
+          "PostPaneTemplate.fxml"));
+      
+    postPaneTemplateLoader.load();
+    PostPaneTemplateController postPaneTemplateController = postPaneTemplateLoader.getController();
+    postPaneTemplateController.setFields(
+          this.primaryStage, this.postPageScene, this.postPageController, this, this.boardAccess);
+    
+    return postPaneTemplateController.setFieldsPostPane(post);
 
-    Label titleLabel = (Label) UiUtils.getNodeFromId(tempChildren, "titleLabel");
-    if (titleLabel != null) {
-      titleLabel.setText(post.getTitle());
-    }
-
-    Label authorLabel = (Label) UiUtils.getNodeFromId(tempChildren, "authorLabel");
-    if (titleLabel != null) {
-      authorLabel.setText("Post by: " + (post.isAnonymous()
-          ? ForumPost.ANONYMOUS_NAME : post.getAuthor().getUsername()));
-    }
-
-    TextArea postTextArea = (TextArea) UiUtils.getNodeFromId(tempChildren, "postTextArea");
-    if (postTextArea != null) {
-      postTextArea.setText(post.getText());
-      postTextArea.setStyle("-fx-opacity: 1;");
-    }
-
-    Label replyLabel = (Label) UiUtils.getNodeFromId(tempChildren, "replyLabel");
-    if (replyLabel != null) {
-      replyLabel.setText(post.getComments().size() + " comments");
-    }
-
-    Label likeLabel = (Label) UiUtils.getNodeFromId(tempChildren, "likeLabel");
-    if (likeLabel != null) {
-      likeLabel.setText(post.getLikes() + " likes");
-    }
-
-    Label dislikeLabel = (Label) UiUtils.getNodeFromId(tempChildren, "dislikeLabel");
-    if (dislikeLabel != null) {
-      dislikeLabel.setText(post.getDislikes() + " dislikes");
-    }
-
-    Button likeButton = (Button) UiUtils.getNodeFromId(tempChildren, "likeButton");
-    Button dislikeButton = (Button) UiUtils.getNodeFromId(tempChildren, "dislikeButton");
-    if (likeButton != null && dislikeButton != null) {
-
-      UiUtils.setStyleOfButton(likeButton,
-          post.getLikeUsers().contains(boardAccess.getActiveUser()));
-      UiUtils.setStyleOfButton(dislikeButton,
-          post.getDislikeUsers().contains(boardAccess.getActiveUser()));
-          
-      likeButton.setOnAction(e -> {
-        try {
-          boardAccess.likePost(post.getId(), boardAccess.getActiveUser());
-        } catch (Exception error) {
-          System.out.println("Klarte ikke like");
-          UiUtils.exceptionAlert(error).showAndWait();
-        }
-        
-        UiUtils.setStyleOfButton(likeButton,
-            post.getLikeUsers().contains(boardAccess.getActiveUser()));
-        UiUtils.setStyleOfButton(dislikeButton,
-            post.getDislikeUsers().contains(boardAccess.getActiveUser()));
-
-        likeLabel.setText(post.getLikes() + " likes");
-        dislikeLabel.setText(post.getDislikes() + " dislikes");
-      });
-
-      dislikeButton.setOnAction(e -> {
-        try {
-          boardAccess.dislikePost(post.getId(), boardAccess.getActiveUser());
-        } catch (Exception error) {
-          UiUtils.exceptionAlert(error).showAndWait();
-        }
-        UiUtils.setStyleOfButton(likeButton,
-            post.getLikeUsers().contains(boardAccess.getActiveUser()));
-        UiUtils.setStyleOfButton(dislikeButton,
-            post.getDislikeUsers().contains(boardAccess.getActiveUser()));
-
-        dislikeLabel.setText(post.getDislikes() + " dislikes");
-        likeLabel.setText(post.getLikes() + " likes");
-      });
-    }
-
-    Button threadButton = (Button) UiUtils.getNodeFromId(tempChildren, "threadButton");
-    if (threadButton != null) {
-      threadButton.setOnAction(e -> {
-        primaryStage = (Stage) createPostButton.getScene().getWindow();
-        primaryStage.setScene(postPageScene);
-        postPageController.drawComments(post.getId());
-        postPageController.reloadPage();
-      });
-    }
-
-    Line titleLine = (Line) UiUtils.getNodeFromId(tempChildren, "titleLine");
-
-    Label timeStampLabel = (Label) UiUtils.getNodeFromId(tempChildren, "timeStampLabel");
-    if (timeStampLabel != null) {
-      timeStampLabel.setText(post.getTimeStamp().toString());
-    }
-
-    toReturn.getChildren().addAll(new ArrayList<Node>(Arrays.asList(titleLabel, authorLabel, 
-          titleLine, postTextArea, likeLabel, dislikeLabel, 
-          replyLabel, likeButton, dislikeButton, threadButton, timeStampLabel)));
-
-    if (boardAccess.getActiveUser() != null 
-          && boardAccess.getActiveUser().equals(post.getAuthor())) {
-      Button deleteButton = (Button) UiUtils.getNodeFromId(tempChildren, "deleteButton");
-      if (deleteButton != null) {
-        deleteButton.setOnAction(e -> {
-          try {
-            boolean confirmation = UiUtils.confimationAlert("Confirm deletion",
-                "Delete post: " + post.getTitle(), "Are you sure you want to delete your post?");
-
-            if (confirmation) {
-              boardAccess.deletePost(post.getId());
-              sortPosts(sortMenuButton.getText());
-            }
-          } catch (Exception e1) {
-            UiUtils.exceptionAlert(e1);
-          }
-        });
-        toReturn.getChildren().add(deleteButton);
-      }
-    }
-    return toReturn;
   }
 }
